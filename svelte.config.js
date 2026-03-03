ea70baab8d26b2afb8219214e9ec4d5cf6d507eb
Michael Walsh <26023962@sun.ac.za>
2026-03-03 22:17:04 +0200

    Actually test this time

--- svelte.config.js
+++ svelte.config.js
@@ -6,14 +6,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
     kit: {
         adapter: adapter({
+            pages: 'build',
+            assets: 'build',
             fallback: '404.html',
             strict: true,
             precompress: true
         }),
         paths: {
-            // base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
             // Sometimes you gotta test with base as not '' cause svelte is terrible with handling these.
-            base: '/some_url'
+            // base: '/some_url'
         },
         prerender: {
             handleHttpError: ({ path }) => {
